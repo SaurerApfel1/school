0953bafa17b4e7c2922a0de57e805f2c56496033
SaurerApfel1 <jonaseliasgoetz@gmail.com>
2024-09-18 21:55:48 +0200

    commit

---
+++ code/Schule/unterricht/steigung.java
@@ -0,0 +1,29 @@
+package Schule.unterricht;
+
+import java.util.Scanner;
+
+public class steigung {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("<<Steigungsberechnung>>");
+
+        System.out.println("Geben Sie die Koordinaten des ersten Punktes an (x1, y1): ");
+        System.out.print("x1:");
+        int x1 = scan.nextInt();
+        System.out.print("y1:");
+        int y1 = scan.nextInt();
+
+
+        System.out.println("Geben Sie die Koordinaten des zweiten Punktes an (x2, y2): ");
+        System.out.print("x2:");
+        int x2 = scan.nextInt();
+        System.out.print("y2:");
+        int y2 = scan.nextInt();
+
+
+        int steigung = (y2-y1)/(x2-x1);
+
+        System.out.println("Die Steigung der Graden beträgt "+steigung+".");
+        }
+    }
