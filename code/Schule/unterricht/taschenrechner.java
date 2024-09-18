e845e2def7631f4ab4990dd32d3b5efc8b4340e9
SaurerApfel1 <jonaseliasgoetz@gmail.com>
2024-09-18 17:39:06 +0200

    commit

--- code/Schule/unterricht/taschenrechner.java
+++ code/Schule/unterricht/taschenrechner.java
@@ -9,36 +9,35 @@ public class taschenrechner {
         System.out.println("<<Taschenrechner>> \n");
         System.out.println("Folgende Programme stehen zur auswahl: \n 1.Addition \n 2.Subtraktion \n 3.Multiplikation \n 4.Division \n 5.Weiter");
         Scanner scan = new Scanner(System.in);
-        System.out.println("Gib eine Zahl zur passenden Ausführung ein:");
+        System.out.print("Gib eine Zahl zur passenden Ausführung ein:");
         int program = scan.nextInt();
 
 
         if (program == 1) {
-            System.out.println("Zahl 1:");
+            System.out.print("Zahl 1:");
             int a = scan.nextInt();
-            System.out.println("Zahl 2:");
+            System.out.print("Zahl 2:");
             int b = scan.nextInt();
             System.out.println("Ergebnis: "+a+"+"+b+"="+(a+b));
         }
-
         else if (program == 2) {
-            System.out.println("Zahl 1:");
+            System.out.print("Zahl 1:");
             int a = scan.nextInt();
-            System.out.println("Zahl 2:");
+            System.out.print("Zahl 2:");
             int b = scan.nextInt();
             System.out.println("Ergebnis: "+a+"-"+b+"="+(a-b));
         }
         else if (program == 3) {
-            System.out.println("Zahl 1:");
+            System.out.print("Zahl 1:");
             int a = scan.nextInt();
-            System.out.println("Zahl 2:");
+            System.out.print("Zahl 2:");
             int b = scan.nextInt();
             System.out.println("Ergebnis: "+a+"*"+b+"="+(a*b));
         }
         else if (program == 4) {
-            System.out.println("Zahl 1:");
+            System.out.print("Zahl 1:");
             int a = scan.nextInt();
-            System.out.println("Zahl 2:");
+            System.out.print("Zahl 2:");
             int b = scan.nextInt();
             System.out.println("Ergebnis: "+a+"/"+b+"="+(a/b));
         }
@@ -49,12 +48,25 @@ public class taschenrechner {
             System.out.println("Gib eine gültige Eingabe ein...");
         }
 
+
         System.out.println("<<Durchschnittsverbrauch>> \n");
-        System.out.println("Geben Sie die gefahrenen Kilometer ein z.B. 200,5 oder 50:");
+        System.out.print("Geben Sie die gefahrenen Kilometer ein z.B. 200,5 oder 50:");
         float km = scan.nextFloat();
-        System.out.println("Geben Sie die verbrauchten Liter ein:");
+        while (km<=0) {
+            System.out.println("Die Kilometer müssen mehr als 0 sein...");
+            System.out.print(">");
+            km = scan.nextInt();
+        }
+
+
+        System.out.print("Geben Sie die verbrauchten Liter ein:");
         float l = scan.nextFloat();
+        while (l<=0) {
+            System.out.println("Der Verbrauch muss mehr als 0 sein...");
+            System.out.print(">");
+            l = scan.nextInt();
+        }
         float durchschnittV = l/km * 100;
-        System.out.println("Du verbrauchst auf 100km "+durchschnittV+"l.");
+        System.out.println("Du verbrauchst auf 100km "+durchschnittV+"L.");
     }
 }
